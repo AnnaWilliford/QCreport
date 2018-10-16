d8208d9eae75583f6200b622b297a8cd707a6783
Chess <awillifo@uta.edu>
2018-10-16 14:09:53 -0500

    add report

---
+++ MakeNozzleReport.R
@@ -0,0 +1,84 @@
+#https://confluence.broadinstitute.org/display/GDAC/Nozzle
+#https://github.com/parklab/nozzle
+
+require( Nozzle.R1 )
+
+args<-commandArgs(TRUE)
+myPath_fastq<-args[1]
+myPath_out<-args[2]
+
+newdir=paste0(myPath_out,"/FastqReports")
+dir.create( newdir, showWarnings=FALSE );
+file.copy("dna_3.png", paste0(newdir,"/dna_3.png"), overwrite=TRUE) 
+
+########### create report elements ###########
+
+report <- newCustomReport( "" );
+myFiles<-newSection("Analyzed files");
+dataOverview <- newSection( "Data overview" );
+qualScores <- newSection("Distribution of quality scores");
+qualScorePos<- newSection("Quality scores across read positions");
+dupLevels<-newSection("Duplication levels");
+references<-newSection("References");
+
+
+########### prepare data to add to report elements ###########
+#----  logo ------------
+html1 <- newHtml("<img src='dna_3.png' alt='' width=100% height=250 >")
+title <- newHtml( "<h1>Basic fastq report</h1>", style="color: #2E2E2E; text-align:left;font-family:courier" )
+
+
+#---- input files -------
+Myfiles_names <- list.files(path=myPath_fastq, pattern="*fastq.gz", full.names=TRUE, recursive=FALSE)
+Myfiles_names<-basename(Myfiles_names)
+Myfiles_names<-as.data.frame(Myfiles_names)
+names(Myfiles_names)<-"Input files"
+t2<-newTable( Myfiles_names);
+
+#----- summary table -----------
+summaryTable<-read.table(paste0(myPath_out,"/Summary.txt"), header=TRUE,quote = "")
+t <- newTable( summaryTable, "Data summary", significantDigits = 4);
+
+#----- figures -----------
+qualFigures<- list.files(path=paste0(myPath_out,"/QualityFig"), pattern="*Quality.png", full.names=TRUE, recursive=FALSE)
+qualPerPosFigures<-list.files(path=paste0(myPath_out,"/QualityPerPosFig"), pattern="*quality.png", full.names=TRUE, recursive=FALSE)
+dupLevelsFigures<-list.files(path=paste0(myPath_out,"/DupLevelsFig"), pattern="*levels.png", full.names=TRUE, recursive=FALSE)
+
+
+# --- References ---
+ref_p1 <- newParagraph( "Tools used to create this report:" );
+
+fastqc_cite <- newCitation( title="FastQC v0.11.7", url="https://www.bioinformatics.babraham.ac.uk/projects/fastqc/" ); 
+fqtools_cite<- newCitation( authors="Alastair P.Droop", title="fqtools: An efficient software suite for modern FASTQ file manipulation", publication="Bioinformatics", issue="12", pages="1883-1884", year="2016", url="https://doi.org/10.1093/bioinformatics/btw088" );
+Nozzle_cite <- newCitation( authors="Nils Gehlenborg", title="Nozzle: a report generation toolkit for data analysis pipelines", publication="Bioinformatics", issue="29", pages="1089-1091", year="2013", url="http://bioinformatics.oxfordjournals.org/content/29/8/1089" );
+
+########## assemble report structure bottom-up ##############3
+myFiles<- addTo(myFiles, t2)
+dataOverview <- addTo( dataOverview, t ); # parent, child_1, ..., child_n 
+
+for (item in qualFigures){
+  item<-paste0("../QualityFig/",basename(item))
+  fig <- newFigure(item, paste0(basename(item),". Click on image to enlarge."))
+  qualScores <- addTo(qualScores,fig);
+}
+
+for (item in qualPerPosFigures){
+  item<-paste0("../QualityPerPosFig/",basename(item))
+  fig <- newFigure(item, paste0(basename(item),". Click on image to enlarge."))
+  qualScorePos <- addTo(qualScorePos,fig);
+}
+
+for (item in dupLevelsFigures){
+  item<-paste0("../DupLevelsFig/",basename(item))
+  fig <- newFigure(item, paste0(basename(item),". Click on image to enlarge."))
+  dupLevels <- addTo(dupLevels,fig);
+}
+
+references<-addTo(references, ref_p1, fastqc_cite, fqtools_cite, Nozzle_cite )
+report <- addTo( report, title, html1, myFiles, dataOverview, qualScores, qualScorePos, dupLevels,references);
+
+############ render report to file #################
+
+writeReport( report,filename=paste0(newdir,"/QCreport"))
+
+
---
+++ QCsummary.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Description: This script uses FastQC, fqtools, Nozzle.R1 R package and in-house Rscripts to generate report
+# with basic quality metrics for every fastq file in a given folder called 'Data_1', for example.
+# All raw data is then written to 'Data_1_FastQC_out' folder that is created in the directory that contains a folder with input fastq.gz files('Data_1').
+# 'Data_1_FastQC_out' folder contains 'FastqReports' folder with QCreport.html for easy viewing of the results. 
+
+
+# Usage: script.sh NameOfFolderWithFastq  #example: ./QCsummary.sh Data_1
+
+FASTQ_DIR=$1  
+
+OUT_DIR=$FASTQ_DIR"_FastQC_out"
+mkdir -p $OUT_DIR
+
+#--------- get QC metrics -------------
+
+echo -e "FileName\tReadCount\tPercentDistinctReads\tReadLength\tQ30" > $OUT_DIR/Summary.txt 
+
+for file in $FASTQ_DIR/*fastq.gz
+	do
+		#gunzip -k $file
+        file_base=$(basename -s .fastq.gz $file) 
+
+		#run fastqc on each file
+		fastqc $file --extract --outdir=$OUT_DIR
+		totalReads=$(grep '^Total Sequences' $OUT_DIR/$file_base"_fastqc"/fastqc_data.txt |awk '{print $3}')
+		collapsedReads=$(grep '^#Total Deduplicated Percentage' $OUT_DIR/$file_base"_fastqc"/fastqc_data.txt |awk '{print substr ($4, 1, 4)}')
+        seqLength=$(grep '^Sequence length' $OUT_DIR/$file_base"_fastqc"/fastqc_data.txt |awk '{print $3}')
+
+		#get distribution of quality scores and Q30
+		fqtools qualtab $file > $OUT_DIR/$file_base.qual.tab		
+        Rscript GetQ30.R $OUT_DIR/$file_base.qual.tab  $OUT_DIR $file_base 
+        
+        Q30=$(cat $OUT_DIR/$file_base"_Q30".txt)
+
+        echo -e "$(basename $file)\t$totalReads\t$collapsedReads\t$seqLength\t$Q30" >>$OUT_DIR/Summary.txt
+				
+done
+
+#--------- rearrange output for writing report --------------
+
+mkdir -p $OUT_DIR/QualityFig
+cp $OUT_DIR/*Quality.png $OUT_DIR/QualityFig
+
+mkdir -p $OUT_DIR/QualityPerPosFig
+mkdir -p $OUT_DIR/DupLevelsFig
+
+for file in $FASTQ_DIR/*fastq.gz
+	do
+        file_base=$(basename -s .fastq.gz $file) 
+        cp $OUT_DIR/$file_base"_fastqc"/Images/per_base_quality.png $OUT_DIR/QualityPerPosFig/$file_base"_per_base_quality.png"
+		cp $OUT_DIR/$file_base"_fastqc"/Images/duplication_levels.png $OUT_DIR/DupLevelsFig/$file_base"_duplication_levels.png"
+done
+
+#--------- remove unused files -----------
+rm -r $OUT_DIR/*fastqc
+rm $OUT_DIR/*tab
+rm $OUT_DIR/*png
+rm $OUT_DIR/*Q30.txt
+
+#--------- write report ------------------
+
+Rscript MakeNozzleReport.R $FASTQ_DIR $OUT_DIR
+
